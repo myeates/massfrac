1d93ad0c503af56d809146cd2914ad9a74a85fd8
Jacob Covington <jacobdotcovington@gmail.com>
2016-06-06 15:19:48 -0400

    Makefile fixed with Dr. Meyer's help. Now grabs codes from online and puts them in an appropriate place

--- Makefile
+++ Makefile
@@ -1,88 +1,27 @@
-#///////////////////////////////////////////////////////////////////////////////
-# <file type="public">
-#
-#   <license>
-#     This file was originally written by Bradley S. Meyer.
-#
-#     This is free software; you can redistribute it and/or modify it
-#     under the terms of the GNU General Public License as published by
-#     the Free Software Foundation; either version 2 of the License, or
-#     (at your option) any later version.
-#
-#     This software is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     Please see the src/README.txt file in this distribution for more
-#     information.
-#   </license>
-#
-# </file>
-#///////////////////////////////////////////////////////////////////////////////
+# simple makefile for a Rappture-based program
 
-SHELL = /bin/sh
-
-#///////////////////////////////////////////////////////////////////////////////
-# The following lines must be edited according to where you have
-# placed your wn_simple_gce module.  You may also
-# edit to choose a different compiler (e.g., g++), to use Valgrind or
-# not, or to profile:
-#///////////////////////////////////////////////////////////////////////////////
-
-GC=g++
-
-#GCEDIR = ../src/
-RAPPDIR = /usr/local/rappture
-VALGRIND= yes
-PROFILE= no
-
-INCLUDES = -I$(RAPPDIR)/include
-LIBS = -L$(RAPPDIR)/lib -lrappture -lm
+RAPPTURE_DIR	= /usr/local/rappture/
+INCLUDES	= -I$(RAPPTURE_DIR)/include
+LIBS		= -L$(RAPPTURE_DIR)/lib -lrappture -lm
 
 VENDORDIR = ./vendor
 OBJDIR = ./obj
 
 include Makefile.inc
 
-CLIBS += $(LIBS)
-CINCLUDE += $(INCLUDES)
+EXEC = compute_species_mass_fraction \
 
-EXEC = \
-  gce_masses \
-  get_omega_from_gas_fraction \
-  gce_with_infall_function \
-  compute_primary_metallicity \
-  compute_species_yield \
-  compute_species_yield_parameters \
-  compute_species_yield_with_user_function \
-  compute_species_mass_fraction \
-  compute_species_mass_fraction_vs_age \
-  compute_species_mass_fraction_with_user_function \
-  radioactivity \
-  remainder \
-  multi_zone_species \
+$(EXEC) : $(WN_OBJ)
+	$(CC) -c $(CFLAGS) $(INCLUDES) -o $(OBJDIR)/$@.o $@.c
+	$(CC) $(WN_OBJ) $(OBJDIR)/$@.o $(CLIBS) -o $@ $(LIBS) $(CLIBS)
 
-.PHONY all : $(EXEC)
+all: $(EXEC)
 
-yes: compute_species_mass_fraction
+cleanruns:
+	$(RM) run*.xml driver*.xml
 
-#===============================================================================
-# Compile codes.
-#===============================================================================
+cleanrappture: cleanruns
+	$(RM) $(EXEC)
 
-$(EXEC) : $(OBJS)
-	$(CC) -c -o $(TMPDIR)$@.o $@.c
-	$(CC) $(OBJS) $(TMPDIR)$@.o  $(CLIBS) -o $@
-
-
-
-.PHONY: clean cleanall
-
-clean: 
-	rm -fr $(TMPDIR)
-
-cleanall: clean
-	rm -f $(EXEC) *.exe
+cleanall: cleanrappture
 	rm -fr $(VENDORDIR) $(OBJDIR)
-	rm -f driver*
--- compute_species_mass_fraction.c
+++ compute_species_mass_fraction.c
@@ -32,139 +32,79 @@
 #include <string.h>
 #include <math.h>
 #include <unistd.h> 
-#include <WnSimpleGce.c>
-#include <my_infall_functions.c>
-#include <my_species_yield_function.c>
+
 #include "rappture.h"
+#include "WnSimpleGce.h"
+
 #include <boost/lexical_cast.hpp>
+
 #define D_INTERVAL 0.1
 
 int main( int argc, char **argv ) {
 
   RpLibrary* lib = NULL;
-  const char* data = NULL;
   char line [100];
   WnSimpleGce *p_model;
   WnSimpleGce__Species *p_species;
   double d_t;
-  int i;
-  int err = 0;
   double k = 0;
   double delta = 0; 
   double omega = 0;
   double alpha = 0;
+  double alpha_i = 0;
   const char* species = NULL;
   double decay_rate = 0;
-  double alpha_i = 0;  
 
-      lib = rpLibrary(argv[1]);
+  lib = rpLibrary(argv[1]);
 
-    if (lib == NULL) {
-        /* cannot open file or out of memory */
-        printf("FAILED creating Rappture Library\n");
-        return(1);
-    }
-
-
-  /*============================================================================
-  // Check input.
-  //==========================================================================*/
-
-//   if ( argc < 8 ) {
-//      fprintf(
-//        stderr,
-//        "\nUsage: %s k Delta omega alpha species decay_rate alpha_i ... \n\n",
-//        argv[0]
-//      );
-//     fprintf(
-//       stderr,
-//        "k = Clayton infall parameter k\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//        "Delta = Clayton infall parameter Delta\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//        "omega = gas consumption rate (per Gyr)\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//       "alpha = primary metallicity yield\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//        "species = name of species\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//        "decay_rate = decay rate (per Gyr)\n\n"
-//      );
-//      fprintf(
-//        stderr,
-//        "alpha_i = alpha parameter for species\n\n"
-//      );
-//      return EXIT_FAILURE;
-//   }
-
-
-  rpGetString(lib,"input.number(k).current",&data);
-  k = rpConvertDbl(data, "points", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(points).current\n");
-        return(2);
+  if( lib == NULL )
+  {
+    /* cannot open file or out of memory */
+    printf("FAILED creating Rappture Library\n");
+    return EXIT_FAILURE;
   }
 
-
-  rpGetString(lib,"input.number(Delta).current",&data);
-  delta = rpConvertDbl(data, "coefficient", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(a).current\n");
-        return(3);
-  }
-  rpGetString(lib,"input.number(omega).current",&data);
-  omega = rpConvertDbl(data, "points", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(points).current\n");
-        return(4);
+  if( rpGetDouble(lib,"input.number(k).current",&k) )
+  {
+    printf ("Error while retrieving k.\n");
+    return EXIT_FAILURE;
   }
 
-  rpGetString(lib,"input.number(alpha).current",&data);
-  alpha = rpConvertDbl(data, "coefficient", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(a).current\n");
-        return(5);
-  }
- rpGetString(lib,"input.number(species).current",&data);
-  species = data;
-  if (species==(NULL)) {
-        printf ("Error while retrieving input.number(points).current\n");
-        return(6);
+  if( rpGetDouble(lib,"input.number(Delta).current",&delta) )
+  {
+    printf ("Error while retrieving Delta.\n");
+    return EXIT_FAILURE;
   }
 
-
-  rpGetString(lib,"input.number(decay_rate).current",&data);
-  decay_rate = rpConvertDbl(data, "coefficient", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(a).current\n");
-        return(7);
-  }
-// rpGetString(lib,"input.integer(points).current",&data);
-//  pts = rpConvertDbl(data, "points", &err);
-//  if (err) {
-//        printf ("Error while retrieving input.number(points).current\n");
-//        return(2);
-//  }
-
-
-  rpGetString(lib,"input.number(alpha_i).current",&data);
-  alpha_i = rpConvertDbl(data, "coefficient", &err);
-  if (err) {
-        printf ("Error while retrieving input.number(a).current\n");
-        return(8);
+  if( rpGetDouble(lib,"input.number(omega).current",&omega) )
+  {
+    printf ("Error while retrieving omega.\n");
+    return EXIT_FAILURE;
   }
 
+  if( rpGetDouble(lib,"input.number(alpha).current",&alpha) )
+  {
+    printf ("Error while retrieving alpha.\n");
+    return EXIT_FAILURE;
+  }
 
+  if( rpGetString(lib,"input.number(species).current", &species ) )
+  {
+    printf ("Error while retrieving species.\n");
+    return EXIT_FAILURE;
+  }
+
+  if( rpGetDouble(lib,"input.number(decay_rate).current",&decay_rate) )
+  {
+    printf ("Error while retrieving decay rate.\n");
+    return EXIT_FAILURE;
+  }
+
+  if( rpGetDouble(lib,"input.number(alpha_i).current",&alpha_i) )
+  {
+    printf ("Error while retrieving alpha_i.\n");
+    return EXIT_FAILURE;
+  }
 
   /*============================================================================
   // Create model.
@@ -200,43 +140,24 @@ int main( int argc, char **argv ) {
   // Get species yield data.
   //==========================================================================*/
   
-  const  char* var[7] = {boost::lexical_cast<std::string>(k).c_str(),boost::lexical_cast<std::string>(delta).c_str(),boost::lexical_cast<std::string>(omega).c_str(),boost::lexical_cast<std::string>(alpha).c_str(),species,boost::lexical_cast<std::string>(decay_rate).c_str(),boost::lexical_cast<std::string>(alpha_i).c_str()};
-  for( i = 0; i <  1; i++ )
-  {
-    if( 
-       WnSimpleGce__Species__updateYieldCoefficient(
-         p_species,
-         (unsigned int) i,
-       atof(var[i+6])
-       ) != 0
-    ) {
-      fprintf( stderr, "Couldn't add yield data!\n" );
-      return EXIT_FAILURE;
-    }
-  }
+  WnSimpleGce__Species__updateYieldCoefficient( p_species, 0, alpha_i );
 
   /*============================================================================
   // Compute species mass fraction.
   //==========================================================================*/
 
-//   fprintf(
-//    stdout,
-//    " t (Gyr)\t  X(%s)\n\n",
-//    WnSimpleGce__Species__getName( p_species )
-//  );
-
   d_t = 0.;
 
   while( d_t < 20.0 + D_INTERVAL ) {
   
-//    fprintf(
-//      stdout,
-//      "%12.6e  %12.6e\n",
-//      d_t,
-//      WnSimpleGce__computeSpeciesMassFraction( p_model, p_species, d_t )
-//      );
-    sprintf(line,"%f %.15f\n",d_t,WnSimpleGce__computeSpeciesMassFraction( p_model, p_species, d_t));
-    rpPutString(lib,"output.curve(tx).component.xy",line,RPLIB_APPEND);
+    sprintf(
+      line,
+      "%f %.15f\n",
+      d_t,
+      WnSimpleGce__computeSpeciesMassFraction( p_model, p_species, d_t)
+    );
+
+    rpPutString( lib,"output.curve(tx).component.xy",line,RPLIB_APPEND );
     d_t += D_INTERVAL;
 
   }
@@ -251,6 +172,7 @@ int main( int argc, char **argv ) {
   /*============================================================================
   // Done.
   //==========================================================================*/
+
   rpResult(lib);
   return EXIT_SUCCESS;
 
